3859f3b403eaa7329ae82c5dd20abb592643514f
yousseffatach <167081833+yousseffatach@users.noreply.github.com>
2024-09-16 18:54:14 +0100

    new files

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,14 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+match priority :
+    case "high":
+        print(f"Reminder: {task} is a high priority task ", end="") 
+    case "medium":
+        print(f"Reminder: {task} is a medium priority task ", end="") 
+    case "low":
+        print(f"Note: {task} is a low priority task. ", end="") 
+
+if time_bound == "yes":
+    print("task that requires immediate attention today!")
+else : print("Consider completing it when you have free time.")
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the size of the pattern:"))
+i=1
+for i in range (0,number) :
+    for j in range (0,number) :
+        print("*", end="")
+    print("")
