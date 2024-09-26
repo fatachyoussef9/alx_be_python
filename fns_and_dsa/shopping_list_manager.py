b77d97ca3521a404e9575bb85d16140ecfdae840
yousseffatach <167081833+yousseffatach@users.noreply.github.com>
2024-09-26 10:56:59 +0100

    int

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -31,13 +31,13 @@ def main():
         display_menu()
         choice = int(input("Enter your choice: "))
 
-        if choice == '1':
+        if choice == 1:
             add_item(shopping_list)
-        elif choice == '2':
+        elif choice == 2:
             remove_item(shopping_list)
-        elif choice == '3':
+        elif choice == 3:
             view_list(shopping_list)
-        elif choice == '4':
+        elif choice == 4:
             print("Goodbye!")
             break
         else:
