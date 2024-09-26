24fc4448518c1d21a16411419193f14f652ccc41
yousseffatach <167081833+yousseffatach@users.noreply.github.com>
2024-09-26 10:30:09 +0100

    new

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,12 @@
 def perform_operation( num1: float, num2: float, operation: str):
     if operation=='add':
-        res = num1 + num2
+        return num1 + num2
     elif operation == 'subtract':
-        res = num1 - num2
+        return num1 - num2
     elif operation == 'multiply': 
-        res = num1 * num2
+        return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Division by zero"
-        else: res = num1 / num2
+        else: return num1 / num2
     else: return "Error: Invalid operation"
-    return res
